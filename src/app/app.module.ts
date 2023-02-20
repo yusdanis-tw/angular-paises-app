85ece244eb1335c468ba439e059d64b311ec252f
Yusdanis Feus <yusdanis.feus@thoughtworks.com>
2023-02-20 13:06:29 -0500

    Primera versión

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { SharedModule } from './shared/shared.module'
+import { PaisModule } from './pais/pais.module'
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PaisModule,
+    HttpClientModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core'
+
+import { PaisService } from '../../services/pais.service'
+import { Country } from '../../interfaces/paises.interface'
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [],
+})
+export class PorPaisComponent {
+  termino: string = ''
+  hayError: boolean = false
+  paises: Country[] = []
+
+  constructor(private paisService: PaisService) {}
+  buscar(termino: string) {
+    this.hayError = false
+    this.termino = termino
+
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises)
+        this.paises = paises
+      },
+      (err) => {
+        this.hayError = true
+        this.paises = []
+      },
+    )
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false
+  }
+}
